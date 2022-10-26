ec2d49d938c7276f35dd9a44fcb60af25908808d
unknown <ainouzusa@gmail.com>
2022-10-26 22:03:07 +0100

    fetchdata

--- src/App.js
+++ src/App.js
@@ -6,7 +6,32 @@ const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  // initialize state
+  const [pokemonList, setpokemonList] = useState([]);
+  const [pokemonFilteredList, setPokemonFilteredList] = useState([]);
 
+  async function fetchData()  {
+    const response = await fetch(pokeApi);
+    const json = await response.json();
+    setpokemonList(json)
+    setPokemonFilteredList(json)}
+
+  // add useEffect here ⬇️
+  useEffect(() => {
+  
+
+  fetchData()
+    
+  }, []);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    const regex = new RegExp(value, 'gi');
+    const filtered = pokemonList.filter((pokemon) => {
+      return pokemon.name.match(regex);
+    });
+    setPokemonFilteredList(filtered);
+  }
   return (
     <div data-testid="app">
       <Navigation />
